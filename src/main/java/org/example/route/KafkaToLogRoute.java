7a8ca4ccd8efcd35b54b24f225a5e462cdf4e320
nholguinrh <nholguin@solucioneshys.net>
2025-04-06 16:49:58 -0500

    first commit

---
+++ src/main/java/org/example/route/KafkaToLogRoute.java
@@ -0,0 +1,17 @@
+package org.example.route;
+
+import org.apache.camel.builder.RouteBuilder;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class KafkaToLogRoute extends RouteBuilder {
+
+    @Override
+    public void configure() {
+        from("kafka:my-topic?brokers=my-cluster-kafka-bootstrap.amq-streams-kafka:9092")
+            .routeId("kafka-jslt-log")
+            .log("Mensaje original desde Kafka: ${body}")
+            .to("jslt:classpath:transformacion.jslt")
+            .log("Mensaje transformado: ${body}");
+    }
+}
